0c93affbb4cde7bdb89356a2dcc7acc3ab6110d9
developingright <mshaheer2003@gmail.com>
2021-10-14 12:03:06 +0530

    Added ModelForm , static files, edit delete and create functionality

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name="todo"),
+    path('edit/<str:pk>/',views.edit,name="edit"),
+    path('delete/<str:pk>/',views.delete,name="delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import *
+from .forms import *
 # Create your views here.
 def index(request):
-    return render(request, 'todo/index.html')
+    items = Task.objects.all()
+    form = TodoForm()
+    if request.method=='POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            print("asdds'")
+            form.save()
+        else:
+            print("asasd")
+        return redirect('/')
+    context = {'items':items,'forms':form}
+    return render(request, 'todo/index.html',context)
+
+def edit(request,pk):
+    item = Task.objects.get(id=pk)
+    form = TodoForm(instance=item)
+    if request.method=='POST':
+        form = TodoForm(request.POST,instance=item)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'form':form}
+    return render(request, 'todo/edit.html',context)
+
+def delete(request,pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method=="POST":
+
+        item.delete()
+        return redirect('/')
+    context = {'item':item}
+    return render(request, 'todo/delete.html',context)
